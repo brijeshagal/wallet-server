6ca2dc388da4bb3910ccc13b4a8658f1985048f2
brijeshagal <brijeshagal@gmail.com>
2025-01-22 03:10:23 +0530

    fix bug

--- src/utils/quote/comparison.ts
+++ src/utils/quote/comparison.ts
@@ -5,11 +5,13 @@ export function getBestQuote(
   successQuotes: ProviderQuoteResponse[],
   inputSrc: InputSrc
 ): ProviderQuoteResponse | undefined {
+  console.log({ successQuotes });
   return successQuotes.reduce((best, current) => {
-    const bestAmount =
-      inputSrc === InputSrc.From
+    const bestAmount = best.from // @dev checks if best is empty or not
+      ? inputSrc === InputSrc.From
         ? BigInt(best.from.amount)
-        : BigInt(best.to.amount);
+        : BigInt(best.to.amount)
+      : 0n;
     const currentAmount =
       inputSrc === InputSrc.From
         ? BigInt(current.from.amount)
